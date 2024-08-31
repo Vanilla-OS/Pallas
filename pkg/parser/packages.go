1abdd5a4858957da3f6a3a20a6740c450fc08c72
mirkobrombin <send@mirko.pm>
2024-09-01 00:27:10 +0200

    fix: exclude root directory from package list to avoid empty items

--- pkg/parser/packages.go
+++ pkg/parser/packages.go
@@ -22,6 +22,11 @@ func GetPackages() ([]string, error) {
 		Mode: packages.NeedFiles, // We only need the file paths
 	}
 
+	rootDir, err := filepath.Abs(".")
+	if err != nil {
+		return nil, err
+	}
+
 	pkgs, err := packages.Load(cfg, "./...")
 	if err != nil {
 		return nil, err
@@ -32,6 +37,11 @@ func GetPackages() ([]string, error) {
 		if len(pkg.GoFiles) > 0 {
 			// To get the package directory, we take the directory of the first Go file
 			dir := filepath.Dir(pkg.GoFiles[0])
+
+			// if the directory is the root of the project, we skip it
+			if dir == rootDir {
+				continue
+			}
 			packageDirs = append(packageDirs, dir)
 		}
 	}
